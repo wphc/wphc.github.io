9403ea3019d827c9c1e79b1d96388d98d0d60a66
wphc <74842047+wphc@users.noreply.github.com>
2020-11-21 19:29:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function statsOver() { // function for when you hover over stats icon
+    document.getElementById("logotext").style.opacity = "0.6";
+    document.getElementById("logotext").innerHTML = "Stats";
+}
+
+function galleryOver() { // function for when you hover over gallery icon
+    document.getElementById("logotext").style.opacity = "0.6";
+    document.getElementById("logotext").innerHTML = "Gallery";
+}
+
+function infoOver() { // function for when you hover over info icon
+    document.getElementById("logotext").style.opacity = "0.6";
+    document.getElementById("logotext").innerHTML = "Info";
+}
+
+function helpOver() { // function for when you hover over help icon
+    document.getElementById("logotext").style.opacity = "0.6";
+    document.getElementById("logotext").innerHTML = "How You Can Help";
+}
+
+function mouseout() { // function for when you hover out of any icon
+    document.getElementById("logotext").style.opacity = "0"; 
+}
+
+
